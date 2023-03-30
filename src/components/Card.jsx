083c5429ec06f82471ba911cd32ffbb5bb30b3c5
Kuba Mrowiec <mruvekss@gmail.com>
2023-03-30 18:29:07 +0200

    Animation update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -15,7 +15,7 @@ const Card = () => {
         <div className="bg-gray-200 space-y-4 p-16">
           <h2 className="text-2xl font-bold">How We Collect: Research, Transparency, and Collaboration</h2>
           <p>Learn about The Met's collecting practices and provenance research.</p>
-          <a href="#" className="hover-underline-animation font-semibold">Read More</a>
+          <a href="#" className="hover-underline-animation-black font-semibold">Read More</a>
         </div>
       </section>
 
@@ -23,7 +23,7 @@ const Card = () => {
         <div className="bg-gray-200 space-y-4 p-16">
           <h2 className="text-2xl font-bold">Women's History Month</h2>
           <p>Celebrate the vital contributions of women artists through exhibitions, talks, digital content, and more.</p>
-          <a href="#" className="hover-underline-animation font-semibold">Learn more</a>
+          <a href="#" className="hover-underline-animation-black font-semibold">Learn more</a>
         </div>
         <img src={card2} alt="" className="w-auto"/>
       </section>
@@ -33,7 +33,7 @@ const Card = () => {
         <div className="bg-gray-200 space-y-4 p-16 ">
           <h2 className="text-2xl font-bold">Art and Activism: Environmental Protection and Contemporary Indigenous Art</h2>
           <p>On Thursday, March 30 join featured artists and the curator of Water Memories and Art of Native America: The Charles and Valerie Diker Collection for a free online conversation.</p>
-          <a href="#" className="hover-underline-animation font-semibold">Register now</a>
+          <a href="#" className="hover-underline-animation-black font-semibold">Register now</a>
         </div>
       </section>
 
@@ -41,7 +41,7 @@ const Card = () => {
         <div className="bg-gray-200 space-y-4 p-16">
           <h2 className="text-2xl font-bold">Date Night at The Met</h2>
           <p>Every Friday and Saturday night enjoy live music, drinks, and gallery chats.</p>
-          <a href="#" className="hover-underline-animation font-semibold">Plan your visit</a>
+          <a href="#" className="hover-underline-animation-black font-semibold">Plan your visit</a>
         </div>
         <img src={card4} alt="" className="w-auto"/>
       </section>
@@ -52,7 +52,7 @@ const Card = () => {
         <div className="bg-gray-200 space-y-4 p-16">
           <h2 className="text-2xl font-bold">Membership at The Museum</h2>
           <p>Museum Members enjoy a rich variety of specialized benefits— from free guest passes to Member Preview Days for new exhibitions—all while supporting our mission.</p>
-          <a href="#" className="hover-underline-animation font-semibold">See the benefits</a>
+          <a href="#" className="hover-underline-animation-black font-semibold">See the benefits</a>
         </div>
       </section>
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,17 +14,16 @@ const Navbar = () => {
   };
 
   return (
-    <nav className="w-full bg-red-600 px-20">
-
+    <nav className="w-full bg-red-800 text-white px-20">
       <div className="flex justify-end">
-        <ul className="flex text-gray-900  font-medium cursor-pointer items-center">
-          <li className="hover:text-gray-200 px-2 border-r border-black">
+        <ul className="flex  font-medium cursor-pointer items-center">
+          <li className="hover: px-2 border-r border-white">
             Buy Tickets
           </li>
-          <li className="hover:text-gray-200 px-2 border-r border-black">
+          <li className="hover: px-2 border-r border-white">
             Become a Member
           </li>
-          <li className="hover:text-gray-200 pl-2 ">
+          <li className="hover: pl-2 ">
             Make A Donation
           </li>
         </ul>
@@ -32,34 +31,34 @@ const Navbar = () => {
 
       
       <ul className="flex justify-start space-x-20 text-xl">
-        <li className="relative my-4 mx-2 text-gray-900 font-medium cursor-pointer hover:text-white rounded-lg">
+        <li className="relative my-4 mx-2  font-medium cursor-pointer hover:text-white rounded-lg">
           <NavLink to="/" ><MdMuseum size={30} /></NavLink>
         </li>
         <li
-          className="relative my-4 mx-2 text-gray-900 font-medium cursor-pointer hover-underline-animation decoration-2"
+          className="relative my-4 mx-2  font-medium cursor-pointer hover-underline-animation decoration-2"
           onMouseEnter={() => handleMouseEnter(1)}
           onMouseLeave={() => handleMouseLeave()}
         >
           <a href="#">Visit</a>
           {activeMenu === 1 && (
-            <div className="absolute z-40 pt-4 text-gray-900 w-max left-0 top-full bg-red-600 shadow-lg">
+            <div className="absolute z-40 pt-4  w-max left-0 top-full bg-red-800 shadow-lg">
               <ul>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <NavLink to="/visit/plan-your-visit">Plan Your Visit</NavLink>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <NavLink to="/visit/buy-tickets">Buy Tickets</NavLink>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <NavLink to="/visit/membership">Become a Member</NavLink>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Museum Map</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <NavLink to="/visit/dining">Food and Drink</NavLink> 
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Accessibility</a>
                 </li>
               </ul>
@@ -67,21 +66,21 @@ const Navbar = () => {
           )}
         </li>
         <li
-          className="relative mx-2 my-4 text-gray-900 font-medium cursor-pointer hover-underline-animation decoration-2"
+          className="relative mx-2 my-4  font-medium cursor-pointer hover-underline-animation decoration-2"
           onMouseEnter={() => handleMouseEnter(2)}
           onMouseLeave={() => handleMouseLeave()}
         >
           <a href="#">Exhibitions and Events</a>
           {activeMenu === 2 && (
-            <div className="absolute z-40 pt-4 text-gray-900 w-max left-0 top-full bg-red-600 shadow-lg">
+            <div className="absolute z-40 pt-4  w-max left-0 top-full bg-red-800 shadow-lg">
               <ul>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <NavLink to="/exhibitions">Exhabitions</NavLink>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Events and Tours</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Performances</a>
                 </li>
               </ul>
@@ -89,21 +88,21 @@ const Navbar = () => {
           )}
         </li>
         <li
-          className="relative mx-2 my-4 text-gray-900 font-medium cursor-pointer hover-underline-animation decoration-2"
+          className="relative mx-2 my-4  font-medium cursor-pointer hover-underline-animation decoration-2"
           onMouseEnter={() => handleMouseEnter(3)}
           onMouseLeave={() => handleMouseLeave()}
         >
           <a href="#">Art</a>
           {activeMenu === 3 && (
-            <div className="absolute z-40 pt-4 text-gray-900 w-max left-0 top-full bg-red-600 shadow-lg">
+            <div className="absolute z-40 pt-4  w-max left-0 top-full bg-red-800 shadow-lg">
               <ul>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Collection</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Curatorial Areas</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Scientific Research</a>
                 </li>
               </ul>
@@ -111,27 +110,27 @@ const Navbar = () => {
           )}
         </li>
         <li
-          className="relative mx-2 my-4 text-gray-900 font-medium cursor-pointer hover-underline-animation decoration-2" 
+          className="relative mx-2 my-4  font-medium cursor-pointer hover-underline-animation decoration-2" 
           onMouseEnter={() => handleMouseEnter(4)}
           onMouseLeave={() => handleMouseLeave()}
         >
           <a href="#">Learn with Us</a>
           {activeMenu === 4 && ( 
-            <div className="absolute z-40 pt-4 text-gray-900 w-max left-0 top-full bg-red-600 shadow-lg">
+            <div className="absolute z-40 pt-4  w-max left-0 top-full bg-red-800 shadow-lg">
               <ul>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Learning Resources</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Publications</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Timeline of Art Histiory</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Workshop and Acitivities</a>
                 </li>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Articles, Videos and Podcasts</a>
                 </li>
               </ul>
@@ -139,22 +138,22 @@ const Navbar = () => {
           )}
         </li>
         <li
-          className="relative mx-2 my-4 text-gray-900 font-medium cursor-pointer hover-underline-animation decoration-2"
+          className="relative mx-2 my-4  font-medium cursor-pointer hover-underline-animation decoration-2"
           onMouseEnter={() => handleMouseEnter(5)}
           onMouseLeave={() => handleMouseLeave()}
         >
           <a href="#">Research</a>
           {activeMenu === 5 && (
-            <div className="absolute z-40 pt-4 text-gray-900 w-max left-0 top-full bg-red-600 shadow-lg">
+            <div className="absolute z-40 pt-4  w-max left-0 top-full bg-red-800 shadow-lg">
               <ul>
-                <li className="hover:bg-red-500 p-2">
+                <li className="hover:bg-red-700 p-2">
                   <a href="#">Libraries and Research Centers</a>
                 </li>
               </ul>
             </div>
           )}
         </li>
-        <li className="relative mx-2 my-4 text-gray-900 font-medium cursor-pointer hover-underline-animation decoration-2">
+        <li className="relative mx-2 my-4  font-medium cursor-pointer hover-underline-animation decoration-2">
           Shop
         </li>
       </ul>
